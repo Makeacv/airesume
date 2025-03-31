766b84065ac4e324dd677d620fc680716e7b2954
Makeacv <info@makeacv.co.za>
2025-03-31 22:36:47 +0200

    Update actions.ts

--- src/app/(main)/editor/forms/actions.ts
+++ src/app/(main)/editor/forms/actions.ts
@@ -19,7 +19,7 @@ export async function generateSummary(input: GenerateSummaryInput) {
     throw new Error("Unauthorized");
   }
 
-  const subscriptionLevel = await getUserSubscriptionLevel(userId);
+  //const subscriptionLevel = await getUserSubscriptionLevel(userId);
 
   if (!canUseAITools()) {
     throw new Error("Upgrade your subscription to use this feature");
